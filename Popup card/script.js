3bd1ad84f8ac6a1cd327bb8a6bcc0ffd93dbdd44
Muhammed Minhaj Mahroof <72432356+minhaaj-t@users.noreply.github.com>
2024-05-19 23:59:48 +0400

    Add files via upload

---
+++ Popup card/script.js
@@ -0,0 +1,18 @@
+//Get refernces to elements
+
+const dialog = document.getElementById("dialog");
+const blurer = document.getElementById("blurer");
+
+//Function to open dialog
+
+function openDialog() {
+  dialog.style.display = "flex";
+  dialog.style.display = "block";
+}
+
+//Fuunction to close dialog
+
+function closeDialog() {
+  dialog.style.display = "none";
+  dialog.style.display = "none";
+}
